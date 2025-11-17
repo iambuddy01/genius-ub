403bf9978e48bff981dd6ae26cb97efac60fb112
ᴅᴇᴠ..!! 🇮🇳 <190914293+karmaxexclusive@users.noreply.github.com>
2025-11-18 01:01:00 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- SHUKLA/plugins/tools/clone.py
+++ SHUKLA/plugins/tools/clone.py
@@ -7,7 +7,7 @@ from SHUKLA.modules.SHASHANK.basic import edit_or_reply, get_text, get_user
 
 
 OWNER = os.environ.get("OWNER", None)
-BIO = os.environ.get("BIO", "ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ sᴛʀᴀɴɢᴇʀ ᴛᴇᴀᴍ ♡ ᴊᴏɪɴ ɴᴏᴡ @ITSZSHUKLA")
+BIO = os.environ.get("BIO", "ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ ɢᴇɴɪᴜs ᴛᴇᴀᴍ ♡ ᴊᴏɪɴ ɴᴏᴡ @fine_n_ok")
 
 
 @app.on_message(cdz(["clone"])  & (filters.me | filters.user(SUDO_USER)))
