883ea0204cd6ade2ef365b2be2952d6c5b5ae0e0
ᴅᴇᴠ..!! 🇮🇳 <190914293+karmaxexclusive@users.noreply.github.com>
2025-11-18 01:07:56 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- SHUKLA/plugins/tools/tts.py
+++ SHUKLA/plugins/tools/tts.py
@@ -10,7 +10,7 @@ def text_to_speech(client, message):
     text = message.text.split(' ', 1)[2]
     tts = gTTS(text=text, lang='hi')
     tts.save('sʜᴜᴋʟᴀ ᴀᴜᴅɪᴏ.mp3')
-    client.send_audio(message.chat.id, 'sʜᴜᴋʟᴀ ᴀᴜᴅɪᴏ.mp3')
+    client.send_audio(message.chat.id, 'ᴀᴜᴅɪᴏ.mp3')
   
 
 __NAME__ = "Tᴛs"
